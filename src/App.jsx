9123b9a5df3a6df023eee77c2d8f3c6cd5e55af3
lhlavati <lukahlavati@gmail.com>
2026-03-13 18:48:57 +0100

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { Container } from 'react-bootstrap'
+import Izbornik from './components/Izbornik'
+import { Route, Routes } from 'react-router-dom'
+import { RouteNames } from './constants'
+import Home from './pages/Home'
+import SmjerPregled from './pages/sportovi/SportPregled'
 
 function App() {
 
+
   return (
-      <Container>
-        
-      </Container>
+    <Container>
+      <Izbornik />
+      <Routes>
+        <Route path={RouteNames.HOME} element={<Home />} />
+        <Route path={RouteNames.SPORTOVI} element={<SmjerPregled />} />
+      </Routes>
+      <hr />
+      &copy; Edunova
+    </Container>
   )
 }
 
---
+++ src/pages/sportovi/SportPregled.jsx
@@ -0,0 +1,7 @@
+export default function SportPregled(){
+    return(
+        <>
+        Ovdje će se vidjeti sportovi
+        </>
+    )
+}
