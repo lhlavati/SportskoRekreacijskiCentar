0c565c13ef5067c6851103614b7d5c9d371a45e0
Luka Hlavati <lukahlavati@gmail.com>
2026-03-17 18:50:32 +0100

    dodani novi atributi na postojeće sportove

--- src/services/sportovi/SportPodaci.js
+++ src/services/sportovi/SportPodaci.js
@@ -1,22 +1,162 @@
 export const sportovi = [
-    { sifra: 1, naziv: 'Nogomet', vrstaTerena: 'trava' },
-    { sifra: 2, naziv: 'Tenis', vrstaTerena: 'zemlja' },
-    { sifra: 3, naziv: 'Padel', vrstaTerena: 'trava' },
-    { sifra: 4, naziv: 'Mali Nogomet', vrstaTerena: 'parket' },
-    { sifra: 5, naziv: 'Košarka', vrstaTerena: 'parket' },
-    { sifra: 6, naziv: 'Odbojka', vrstaTerena: 'parket' },
-    { sifra: 7, naziv: 'Rukomet', vrstaTerena: 'parket' },
-    { sifra: 8, naziv: 'Vaterpolo', vrstaTerena: 'voda' },
-    { sifra: 9, naziv: 'Plivanje', vrstaTerena: 'voda' },
-    { sifra: 10, naziv: 'Odbojka na pijesku', vrstaTerena: 'pijesak' },
-    { sifra: 11, naziv: 'Stolni tenis', vrstaTerena: 'stol' },
-    { sifra: 12, naziv: 'Badminton', vrstaTerena: 'parket' },
-    { sifra: 13, naziv: 'Boks', vrstaTerena: 'ring' },
-    { sifra: 14, naziv: 'Judo', vrstaTerena: 'tatami' },
-    { sifra: 15, naziv: 'Američki nogomet', vrstaTerena: 'trava' },
-    { sifra: 16, naziv: 'Bejzbol', vrstaTerena: 'pijesak/trava' },
-    { sifra: 17, naziv: 'Hokej na ledu', vrstaTerena: 'led' },
-    { sifra: 18, naziv: 'Golf', vrstaTerena: 'trava' },
-    { sifra: 19, naziv: 'Skvoš', vrstaTerena: 'parket' },
-    { sifra: 20, naziv: 'Kuglanje', vrstaTerena: 'staza' }
+    { 
+        sifra: 1, 
+        naziv: 'Nogomet', 
+        vrstaTerena: 'trava', 
+        kontaktni: true, 
+        maxIgraca: 22, 
+        unutra: false 
+    },
+    { 
+        sifra: 2, 
+        naziv: 'Tenis', 
+        vrstaTerena: 'zemlja', 
+        kontaktni: false, 
+        maxIgraca: 4, 
+        unutra: false
+    },
+    { 
+        sifra: 3, 
+        naziv: 'Padel', 
+        vrstaTerena: 'trava', 
+        kontaktni: false, 
+        maxIgraca: 4, 
+        unutra: true 
+    },
+    { 
+        sifra: 4, 
+        naziv: 'Mali Nogomet', 
+        vrstaTerena: 'parket', 
+        kontaktni: true, 
+        maxIgraca: 12, 
+        unutra: true 
+    },
+    { 
+        sifra: 5, 
+        naziv: 'Košarka', 
+        vrstaTerena: 'parket', 
+        kontaktni: true, 
+        maxIgraca: 10, 
+        unutra: true 
+    },
+    { 
+        sifra: 6, 
+        naziv: 'Odbojka', 
+        vrstaTerena: 'parket', 
+        kontaktni: false, 
+        maxIgraca: 12, 
+        unutra: true 
+    },
+    { 
+        sifra: 7, 
+        naziv: 'Rukomet', 
+        vrstaTerena: 'parket', 
+        kontaktni: true, 
+        maxIgraca: 14, 
+        unutra: true 
+    },
+    { 
+        sifra: 8, 
+        naziv: 'Vaterpolo', 
+        vrstaTerena: 'voda', 
+        kontaktni: true, 
+        maxIgraca: 14, 
+        unutra: true 
+    },
+    { 
+        sifra: 9, 
+        naziv: 'Plivanje', 
+        vrstaTerena: 'voda', 
+        kontaktni: false, 
+        maxIgraca: 8, 
+        unutra: true 
+    },
+    { 
+        sifra: 10, 
+        naziv: 'Odbojka na pijesku', 
+        vrstaTerena: 'pijesak', 
+        kontaktni: false, 
+        maxIgraca: 4, 
+        unutra: false 
+    },
+    { 
+        sifra: 11, 
+        naziv: 'Stolni tenis', 
+        vrstaTerena: 'stol', 
+        kontaktni: false, 
+        maxIgraca: 4, 
+        unutra: true 
+    },
+    { 
+        sifra: 12, 
+        naziv: 'Badminton', 
+        vrstaTerena: 'parket', 
+        kontaktni: false, 
+        maxIgraca: 4, 
+        unutra: true 
+    },
+    { 
+        sifra: 13, 
+        naziv: 'Boks', 
+        vrstaTerena: 'ring', 
+        kontaktni: true, 
+        maxIgraca: 2, 
+        unutra: true 
+    },
+    { 
+        sifra: 14, 
+        naziv: 'Judo', 
+        vrstaTerena: 'tatami', 
+        kontaktni: true, 
+        maxIgraca: 2, 
+        unutra: true 
+    },
+    { 
+        sifra: 15, 
+        naziv: 'Američki nogomet', 
+        vrstaTerena: 'trava', 
+        kontaktni: true, 
+        maxIgraca: 22, 
+        unutra: false 
+    },
+    { 
+        sifra: 16, 
+        naziv: 'Bejzbol', 
+        vrstaTerena: 'pijesak/trava', 
+        kontaktni: false, 
+        maxIgraca: 18, 
+        unutra: false 
+    },
+    { 
+        sifra: 17, 
+        naziv: 'Hokej na ledu', 
+        vrstaTerena: 'led', 
+        kontaktni: true, 
+        maxIgraca: 12, 
+        unutra: true 
+    },
+    { 
+        sifra: 18, 
+        naziv: 'Golf', 
+        vrstaTerena: 'trava', 
+        kontaktni: false, 
+        maxIgraca: 4, 
+        unutra: false 
+    },
+    { 
+        sifra: 19, 
+        naziv: 'Skvoš', 
+        vrstaTerena: 'parket', 
+        kontaktni: false, 
+        maxIgraca: 2, 
+        unutra: true 
+    },
+    { 
+        sifra: 20, 
+        naziv: 'Kuglanje', 
+        vrstaTerena: 'staza', 
+        kontaktni: false, 
+        maxIgraca: 8, 
+        unutra: true 
+    }
 ];
