85e3d2711d9ed7dec2e748a902f31b79d4a303a9
Luka Hlavati <lukahlavati@gmail.com>
2026-03-19 17:59:27 +0100

    tablica sportovima i react icons

--- src/pages/sportovi/SportPregled.jsx
+++ src/pages/sportovi/SportPregled.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import SportService from "../../services/sportovi/SportService"
+import { Table } from "react-bootstrap"
+import { FaCheckCircle, FaMinusCircle } from "react-icons/fa"
 
 export default function SportPregled(){
 
@@ -15,13 +17,42 @@ export default function SportPregled(){
         })
     }
 
+    function checkKontaktni(kontaktni) {
+        if(kontaktni){
+            return <FaCheckCircle size={25} color="green"/>
+        } else {
+            return <FaMinusCircle size={25} color="red"/>
+        }
+    }
+
     return(
         <>
-            <ul>
-                {sportovi && sportovi.map((sport) => (
-                    <li>{sport.naziv}</li>
-                ))} 
-            </ul>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Naziv</th>
+                        <th>Vrsta Terena</th>
+                        <th>Kontaktni</th>
+                        <th>Max igrača</th>
+                        <th>Unutra</th>
+                        <th>Akcija</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {sportovi && sportovi.map((sport) => (
+                        <tr>
+                            <td>{sport.naziv}</td>
+                            <td>{sport.vrstaTerena}</td>
+                            <td>
+                                {checkKontaktni(sport.kontaktni)}
+                            </td>
+                            <td>{sport.maxIgraca}</td>
+                            <td>{sport.unutra}</td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </>
     )
 }
