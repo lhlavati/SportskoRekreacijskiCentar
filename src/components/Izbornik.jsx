36d768cf4e472e06b7c5a263f9de618da1ef8210
lhlavati <lukahlavati@gmail.com>
2026-05-14 18:28:09 +0200

    logo update

--- src/components/Izbornik.jsx
+++ src/components/Izbornik.jsx
@@ -1,7 +1,7 @@
 import { Container, Image, Nav, Navbar, NavDropdown, Button } from "react-bootstrap";
 import { RouteNames } from "../constants";
 import { useNavigate } from "react-router-dom";
-import logo from '../assets/logogemini.png'
+import logo from '../assets/logo.png'
 import useAuth from "../hooks/useAuth";
 
 
